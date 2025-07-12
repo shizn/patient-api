e9765dfcddaadeed37e070d6e521ebecf25500b4
Xin Shi <xin.shi@pingcap.com>
2025-07-11 22:05:16 -0700

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -7,5 +7,5 @@ dotenv.config();
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
